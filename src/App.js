199ae6296e82a473ff93a13a42397c5453bcbc04
Brian Jacobel <bjacobel@gmail.com>
2016-12-08 11:03:53 -0500

    Renders data to a DOM node passed with ID

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { getAccount } from './services/accounts';
 import './App.css';
 
 class App extends Component {
+  componentWillMount() {
+    getAccount().then((data) => {
+      this.setState({ data });
+    });
+  }
+
   render() {
+    let data = {};
+
+    if (this.state) {
+      data = this.state.data;
+    }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <pre>{ JSON.stringify(data, null, 2) }</pre>
     );
   }
 }
