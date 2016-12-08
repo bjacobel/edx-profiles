b93fa55acf4148db13c06f1d699bbd121877b4bd
Brian Jacobel <bjacobel@gmail.com>
2016-12-08 16:52:29 -0500

    Add a textual indicator of progress

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -7,10 +7,12 @@ class ProgressBar extends Component {
     const percentComplete = this.props.percentage * 100 + '%',
           percentIncomplete = 100 - (this.props.percentage * 100) + '%';
     return (
-      <div className="progress-bar">
-        <span className="sr">{percentComplete} complete</span>
-        <span className="progress-complete" style={{width: percentComplete}}></span>
-        <span className="progress-incomplete" style={{width: percentIncomplete}}></span>
+      <div>
+        <label className="progress-description" htmlFor="progress-bar">Profile: <b>{ percentComplete } Complete</b></label>
+        <div className="progress-bar" id="progress-bar">
+          <span className="progress-complete" style={{width: percentComplete}}></span>
+          <span className="progress-incomplete" style={{width: percentIncomplete}}></span>
+        </div>
       </div>
     );
   }
