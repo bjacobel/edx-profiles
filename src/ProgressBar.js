1bc279a518d58e588f3a499ce6133ff3c58e51a9
Ari Rizzitano <arizzitano@edx.org>
2016-12-08 15:11:12 -0500

    add progress bar styling

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+import './ProgressBar.css';
+
 class ProgressBar extends Component {
   render() {
+  	const percentComplete = this.props.percentage * 100 + '%',
+  				percentIncomplete = 100 - (this.props.percentage * 100) + '%';
     return (
-      <div className="progress-bar">i am a progress bar</div>
+      <div className="progress-bar">
+      	<span className="progress-complete" style={{width: percentComplete}}></span>
+      	<span className="progress-incomplete" style={{width: percentIncomplete}}></span>
+      </div>
     );
   }
 }
+
+ProgressBar.propTypes = {
+	percentage: React.PropTypes.number
+};
 
 export default ProgressBar;
